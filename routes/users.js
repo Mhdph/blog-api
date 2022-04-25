4c8ed713ef01d58ddd1a081b3a5ce4eb36fd59b6
mahdi parivash <mahdiparivash@protonmail.com>
2022-04-25 13:40:06 +0430

    add user route

--- routes/users.js
+++ routes/users.js
@@ -3,32 +3,31 @@ const User = require("../models/User");
 const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
-//Update
+//UPDATE
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       req.body.password = await bcrypt.hash(req.body.password, salt);
-
-      try {
-        const updatedUser = await User.findByIdAndUpdate(
-          req.params.id,
-          {
-            $set: req.body,
-          },
-          { new: true }
-        );
-        res.status(200);
-      } catch (error) {
-        res.status(500).json(error);
-      }
-    } else {
-      res.status(401).json("You can Update only Your account");
     }
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(401).json("You can update only your account!");
   }
 });
 
-//Delete
+//DELETE
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
     try {
@@ -45,6 +44,17 @@ router.delete("/:id", async (req, res) => {
     }
   } else {
     res.status(401).json("You can delete only your account!");
+  }
+});
+
+//GET USER
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
