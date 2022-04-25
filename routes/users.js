6a552e74075450b03e6578ec3cae4f2f17415e4c
mahdi parivash <mahdiparivash@protonmail.com>
2022-04-25 13:28:43 +0430

    update user route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 //Update
@@ -10,9 +11,13 @@ router.put("/:id", async (req, res) => {
       req.body.password = await bcrypt.hash(req.body.password, salt);
 
       try {
-        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-          $set: req.body,
-        });
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
         res.status(200);
       } catch (error) {
         res.status(500).json(error);
@@ -24,11 +29,22 @@ router.put("/:id", async (req, res) => {
 });
 
 //Delete
-
 router.delete("/:id", async (req, res) => {
-  try {
-  } catch (error) {
-    res.status(500).json(error);
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    res.status(401).json("You can delete only your account!");
   }
 });
 
