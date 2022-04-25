0535e924607bef468b07943218490f8f7aa4e271
mahdi parivash <mahdiparivash@protonmail.com>
2022-04-25 14:48:12 +0430

    Multer added To Project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const app = express();
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
@@ -14,6 +15,20 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Connected to mongodb"))
   .catch((err) => console.log("Could not connect to mongodb"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("fiele has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
